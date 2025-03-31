eeaff6f371931a9255928e9a4f8514a498b19a9e
NyasimiPhilip <nyasimiphilip@gmail.com>
2025-03-31 08:24:59 +0300

    Update

--- school_admin/asgi.py
+++ school_admin/asgi.py
@@ -8,14 +8,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
+
+# Set the Django settings module before any Django imports
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_admin.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from admin_interface.routing import websocket_urlpatterns
 from admin_interface.middleware import WebSocketJWTAuthMiddleware
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_admin.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
